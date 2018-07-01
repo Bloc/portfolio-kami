a3003b5f0662c985029a244f3da17e98b730ca73
Avi Warner <aviwarner@gmail.com>
2018-06-30 20:20:22 -0700

    Added wawametrics snippet and events

--- js/main.js
+++ js/main.js
@@ -21,4 +21,13 @@ Array.prototype.forEach.call(elements, function(element) {
   }
 });
 
-console.log('page loaded');
+let blocmetrics = {};
+
+blocmetrics.report = (eventName) => {
+  let event = { event: { name: eventName } };
+  let request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:3000/api/events", true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  console.log(event);
+  request.send(JSON.stringify(event));
+}
